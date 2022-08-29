528b90581a4aa640c40c2ed4fffd6c3ffe002f29
Jeffrey Tarazona <jefftorres021@gmail.com>
2022-08-29 15:44:17 -0500

    Added get & post api methods

---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Product>> GetAll()
+        {
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            return Products.Single(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product model)
+        {
+            model.Id = Products.Count() + 1;
+            Products.Add(model);
+
+            return CreatedAtAction("Get", new { id = model.Id }, model);
+        }
+
+        private static readonly List<Product> Products = new()
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Guitarra eléctrica",
+                Price = 1200,
+                Description = "Ideal para tocar jazz, blues, rock clásico y afines."
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Amplificador para guitarra eléctrica",
+                Price = 699,
+                Description = "Excelente amplificador con un sonido cálido."
+            }
+        };
+
+    }
+}
